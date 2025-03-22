94faa0944e414587692d71d7172a47eb1329ea28
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-03-22 22:29:03 +0700

    Update UserApplication

--- user/src/main/java/org/example/user/UserApplication.java
+++ user/src/main/java/org/example/user/UserApplication.java
@@ -1,7 +1,5 @@
 package org.example.user;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
